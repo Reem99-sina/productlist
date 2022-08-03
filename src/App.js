5ea07b33e2dc6203bbaaa624f7351f4e8056e142
Reem99-sina <reemsina2@gmail.com>
2022-08-03 16:15:49 +0200

    so

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header';
-import { Route, Routes, useNavigate } from 'react-router-dom'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import ProductList from './components/ProductList';
 import ProductDetials from './components/ProductDetials';
 import Register from './components/Register';
@@ -23,6 +23,13 @@ function App() {
     setuserData(null)
     navigate('/login')
   }
+  function Forwardorback({ children }) {
+    if (!localStorage.getItem("userToKen")) {
+      <Navigate to='/login' />
+    } else {
+      return children
+    }
+  }
   return (<>
     <Header logout={logout} userData={userData} />
     <Routes>
@@ -30,8 +37,8 @@ function App() {
       <Route path='/register' element={<Register />} />
       <Route path='/login' element={<Login getUserData={getUserData} />} />
       <Route path='/productlist' element={<ProductList />} />
-      <Route path='/cartPage' element={<Cartpage />} />
-      <Route path='product/:productId' element={<ProductDetials />} />
+      <Route path='/cartPage' element={<Forwardorback><Cartpage /></Forwardorback>} />
+      <Route path='product/:productId' element={<Forwardorback><ProductDetials /></Forwardorback>} />
       <Route path='*' element={<h3>404</h3>} />
     </Routes>
   </>
