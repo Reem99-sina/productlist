b5235709af1aa04226117e58fc017dcf9d1435a0
Reem99-sina <reemsina2@gmail.com>
2022-12-08 12:50:23 +0200

    so

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header(props) {
         </Helmet>
         <div className="navbar navbar-expand-md navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" >fake shop</Link>
+                <Link className="navbar-brand" to='/productlist/products'>fake shop</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
